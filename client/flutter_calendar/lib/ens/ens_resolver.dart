132bbf512f4a8ecb0ec966026c229dff0482aa99
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2022-12-03 02:51:17 +0530

    add getENS function

--- client/flutter_calendar/lib/ens/ens_resolver.dart
+++ client/flutter_calendar/lib/ens/ens_resolver.dart
@@ -1,12 +1,28 @@
 import 'package:ens_dart/ens_dart.dart';
 import 'package:flutter_calendar/web3/web3_client.dart';
+import 'package:velocity_x/velocity_x.dart';
 import 'package:web3dart/web3dart.dart';
 
 mixin ENSResolver {
+  static final Ens ens = Ens(client: Web3ClientDart.client);
   static Future<String> resolveENS(String ensName) async {
-    final Ens ens = Ens(client: Web3ClientDart.client);
     final EthereumAddress ethereumAddress =
         await ens.withName(ensName).getAddress();
     return ethereumAddress.hex;
+  }
+  
+  /// Returns null if no ENS is registered
+  static Future<String?> getENS(String address) async {
+    try {
+      final EthereumAddress ethereumAddress = EthereumAddress.fromHex(address);
+      final String resolvedEns =
+          await ens.withAddress(ethereumAddress).getName();
+      return resolvedEns;
+    } catch (e, stackTrace) {
+      Vx.log(
+        "ReverseENS: $e, ${stackTrace.toString()}",
+      );
+      return null;
+    }
   }
 }
