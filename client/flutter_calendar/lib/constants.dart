3ba0d3254ea59e342b78f20aa85b3638515e5b93
anshul-frontier <anshul@frontier.xyz>
2022-12-03 16:15:07 +0530

    add: share link formation

--- client/flutter_calendar/lib/constants.dart
+++ client/flutter_calendar/lib/constants.dart
@@ -1,4 +1,6 @@
 class SyncConstant {
   static const String privateKeyBox = "private_key_box";
   static const String privateKeyStorageKey = "private_key_storage";
+  static const String iosAppendLink = "sync://address=";
+  static const String androidAppendLink = "https://sync.com?address=";
 }
